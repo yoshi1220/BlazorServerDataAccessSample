b1c2393c29bee1adb8529b28ac7fc205ea0ae99a
yoshi1220 <19341602+yoshi1220@users.noreply.github.com>
2022-02-25 15:24:48 +0900

    SeedData

--- BlazorServerDataAccessSample/Data/SampleDbContext.cs
+++ BlazorServerDataAccessSample/Data/SampleDbContext.cs
@@ -14,5 +14,38 @@ namespace BlazorServerDataAccessSample.Data
         public SampleDbContext(DbContextOptions<SampleDbContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .ToTable("Users");
+            modelBuilder.Entity<User>()
+                .Property(s => s.UserName)
+                .IsRequired(false);
+            modelBuilder.Entity<User>()
+                .HasData(
+                    new User
+                    {
+                        UserId = 1,
+                        UserName = "山田太郎",
+                        MailAddress = "yamada@mai.com",
+                        BirthDay = new DateTime(2001, 1, 1)
+                    },
+                    new User
+                    {
+                        UserId = 2,
+                        UserName = "小鳥遊咲",
+                        MailAddress = "takanashi@mail.com",
+                        BirthDay = new DateTime(2009, 12, 21)
+                    },
+                    new User
+                    {
+                        UserId = 3,
+                        UserName = "山本杏里",
+                        MailAddress = "anri@mail.com",
+                        BirthDay = new DateTime(2004, 7, 11)
+                    }
+                );
+        }
     }
 }
---
+++ BlazorServerDataAccessSample/Migrations/20220225062210_SeedData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorServerDataAccessSample.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "BirthDay", "MailAddress", "UserName" },
+                values: new object[] { 1, new DateTime(2001, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "yamada@mai.com", "山田太郎" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "BirthDay", "MailAddress", "UserName" },
+                values: new object[] { 2, new DateTime(2009, 12, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "takanashi@mail.com", "小鳥遊咲" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "BirthDay", "MailAddress", "UserName" },
+                values: new object[] { 3, new DateTime(2004, 7, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), "anri@mail.com", "山本杏里" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
